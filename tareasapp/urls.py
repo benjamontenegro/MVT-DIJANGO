5f73a52e53adc2a949a7dd02dca91385c4ddebea
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-25 09:57:33 -0300

    hecho el maldito crud
    uwu

--- tareasapp/urls.py
+++ tareasapp/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
 from . import views
 
+# RUTAS CON SUS FUNCIONES  Y TOA LA WEA
+# ASKBAKSBDASKDB
 
 urlpatterns = [
-    path('', views.inicio, name= 'inicio'),
-    path('crear/', views.crear_tarea, name= 'crear_tarea'),
+    path(
+        '', 
+        views.inicio, 
+        name= 'inicio'
+    ),
+    path(
+        'crear/', 
+        views.crear_tarea, 
+        name= 'crear_tarea'
+    ),
+    path(
+        'detalle/<int:id>',
+        views.detalle_tarea, 
+        name= 'detalle_tarea'
+    ),
+    path(
+        'editar/<int:id>',
+        views.editar_tarea,
+        name= 'editar_tarea'
+    ),
+    path(
+        'eliminar/<int:id>',
+        views.eliminar_tarea,
+        name= 'eliminar_tarea'
+    )
 ]
--- tareasapp/views.py
+++ tareasapp/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from .models import Tarea
 # Create your views here.
 
-# def inicio(request):
-#     return HttpResponse("Hola, esta es mi App de Tareas")
+# Esta es la logica de toda la aplicaicon
 
 def inicio(request):
     tareas = Tarea.objects.all()
@@ -17,7 +16,7 @@ def crear_tarea(request):
     if request.method == 'POST':
         titulo = request.POST['titulo']
         descripcion = request.POST['descripcion']
-        completada = 'completa' in request.POST
+        completada = 'completada' in request.POST
 
         Tarea.objects.create(
             titulo = titulo,
@@ -28,3 +27,39 @@ def crear_tarea(request):
         return redirect('inicio')
         # LLEVA AL INICIO
     return render (request, 'tareasapp/crear.html')
+
+# MOSTRAR URL DE DETALLE TAREA CON LA ID DE LA TAREA
+# A DETALLAR
+def detalle_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+    
+    return render(request, 'tareasapp/detalle.html', {
+        'tarea': tarea
+    })
+
+# EDITAR TAREA CON EL ID
+def editar_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+    
+    if request.method == 'POST':
+        tarea.titulo = request.POST['titulo']
+        tarea.descripcion = request.POST['descripcion']
+        # LOS TRUEFALSE SIEMPRE EMPIEZAN EN FALSO
+        tarea.completada = 'completada' in request.POST
+        tarea.save()
+        return redirect('inicio')
+    
+    return render(request, 'tareasapp/editar.html', {
+        'tarea' : tarea
+    })
+
+def eliminar_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+
+    if request.method == 'POST':
+        tarea.delete()
+        return redirect('inicio')
+    
+    return render(request, 'tareasapp/eliminar.html', {
+        'tarea': tarea
+    })
